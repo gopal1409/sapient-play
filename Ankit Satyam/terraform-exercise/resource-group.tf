d771cc3de1623ab34187e94547aaf1c7abcaf8dc
anksatya <ankit.satyam@publicissapient.com>
2023-01-04 17:28:12 +0530

    terraform exercise

---
+++ Ankit Satyam/terraform-exercise/resource-group.tf
@@ -0,0 +1,12 @@
+resource "random_string" "myrandom" {
+  length  = 5
+  upper   = false
+  special = false
+  numeric = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.resource_name_prefix}-${var.resource_group}-${random_string.myrandom.id}"
+  location = var.resource_group_location
+  tags     = local.common_tags
+}
