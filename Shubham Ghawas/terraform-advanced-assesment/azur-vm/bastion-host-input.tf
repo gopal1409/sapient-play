7fb147f53a823f4daf853f3ee0fd96b0eaa29260
shubham ghawas <shubhamghavas@gmail.com>
2023-01-06 18:59:08 +0530

    terraform

---
+++ Shubham Ghawas/terraform-advanced-assesment/azur-vm/bastion-host-input.tf
@@ -0,0 +1,23 @@
+variable "bastion_service_subnet_name" {
+  type    = string
+  default = "AzureBastionSubnet"
+}
+variable "bastion_service_address_prefixes" {
+  type    = list(string)
+  default = ["10.0.101.0/27"]
+}
+
+variable "bastion_subnet_name" {
+  type    = string
+  default = "bastionsubnet"
+}
+
+variable "bastion_subnet_address" {
+  type    = list(string)
+  default = ["10.0.100.0/24"]
+}
+
+variable "source_address_range" {
+  type    = string
+  default = "172.173.236.101/32"
+}
