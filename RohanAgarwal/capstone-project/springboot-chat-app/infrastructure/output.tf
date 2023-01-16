1c7713ab4a8f5bfd0137f92935c84fea51f70e8c
Rohan Agarwal <rohagarw4@publicisgroupe.net>
2023-01-16 22:16:16 +0530

    project

--- RohanAgarwal/capstone-project/springboot-chat-app/infrastructure/output.tf
+++
@@ -1,9 +0,0 @@
-output "web_linuxvm_private_ip_address" {
-  description = "web linux vm private ip"
-  value       = azurerm_linux_virtual_machine.webserver.private_ip_address
-}
-
-output "web_linuxvm_public_ip_address" {
-  description = "web linux vm public ip"
-  value       = azurerm_linux_virtual_machine.webserver.public_ip_address
-}
